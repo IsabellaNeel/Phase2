1a882bcc04542771ea98786b624b73b4fd91f4e0
Isabella Neel <ineel@terpmail.umd.edu>
2023-10-17 17:27:24 -0400

    added user.js

---
+++ User.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(userName, foodArr) {
+        this.userName = userName;
+        this.foodArr = foodArr;
+    }
+
+    addFoodItem(foodItem){
+        this.foodArr.push(foodItem);
+    }
+
+    getFoodArr(){
+        return this.foodArr;
+    }
+
+    getUserName(){
+        return this.userName;
+    }
+}
