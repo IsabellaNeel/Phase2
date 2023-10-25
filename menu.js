30fc4d74e90cc3485aa9dec7c23f82ead8ce28fc
Andrew <andrewjin@outlook.com>
2023-10-24 20:08:49 -0400

    menu changes and adapted shawdi's recipe stuff into my branch

--- menu.js
+++ menu.js
@@ -1,9 +1,21 @@
 var toggleMenu = function () {
-    if (menu.className === "Menu show") {
-        menu.className = "Menu hide";
-        hamburger.className = "hamburger";
+    if (menu.className === "menu show") {
+        menu.className = "menu hide";
+        hamburger.className = "menuicon";
     } else {
-        menu.className = "Menu show";
-        hamburger.className = "hamburger toggled";
+        menu.className = "menu show";
+        hamburger.className = "menuicon toggled";
+    }
+};
+
+var togglePopup = function () {
+    if (popup.className === "popup pshow" || overlay.className === "overlay pshow") {
+        popup.className = "popup";
+        overlay.className = "overlay";
+        plus.className = "menuicon";
+    } else {
+        popup.className = "popup pshow";
+        overlay.className = "overlay oshow";
+        plus.className = "menuicon toggled";
     }
 };
