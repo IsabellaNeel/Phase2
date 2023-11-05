1366167c33daff431578967b6e7656bb18892c5c
Andrew <andrewjin@outlook.com>
2023-11-05 15:19:12 -0500

    recipe tab changes

--- Functionality.js
+++ Functionality.js
@@ -321,128 +321,8 @@ function getFoodInfo(index){ //gets information about specified food item to dis
 /*YOUR KITCHEN FUNCTIONS END*/
 
 /* recipe functions start*/
-var toggleMenu = function () {
-    if (menu.className === "menu show") {
-        menu.className = "menu hide";
-        hamburger.className = "menuicon";
 
-        // close all others
-        plus.className = "menuicon";
-        popup.className = "popup";
-        overlay.className = "overlay";
-        popupMeals.className = "popup";
-        overlayMeals.className = "overlay";
-        popupFlavors.className = "popup";
-        overlayFlavors.className = "overlay";
-        popupPersons.className = "popup";
-        overlayPersons.className = "overlay";
-
-    } else {
-        menu.className = "menu show";
-        hamburger.className = "menuicon toggled";
-
-        // close all others
-        plus.className = "menuicon";
-        popup.className = "popup";
-        overlay.className = "overlay";
-    }
-
-};
-
-var togglePopup = function () {
-    if (popup.className === "popup pshow" || overlay.className === "overlay pshow") {
-        popup.className = "popup";
-        overlay.className = "overlay";
-        plus.className = "menuicon";
-
-        // close all others
-        menu.className = "menu hide";
-        hamburger.className = "menuicon";
-
-    } else {
-        popup.className = "popup pshow";
-        overlay.className = "overlay oshow";
-        plus.className = "menuicon toggled";
-        
-        // close all others
-        menu.className = "menu hide";
-        hamburger.className = "menuicon";
-        popupPersons.className = "popup";
-        overlayPersons.className = "overlay";
-        popupMeals.className = "popup";
-        overlayMeals.className = "overlay";
-        popupFlavors.className = "popup";
-        overlayFlavors.className = "overlay";
-    }
-};
-
-var togglePopupPersons = function () {
-    if (popupPersons.className === "popup pshow" || overlayPersons.className === "overlay pshow") {
-        popupPersons.className = "popup";
-        overlayPersons.className = "overlay";
-    } else {
-        popupPersons.className = "popup pshow";
-        overlayPersons.className = "overlay oshow";
-        
-        // close all others
-        plus.className = "menuicon";
-        popup.className = "popup";
-        overlay.className = "overlay";
-        popupMeals.className = "popup";
-        overlayMeals.className = "overlay";
-        popupFlavors.className = "popup";
-        overlayFlavors.className = "overlay";
-    }
-};
-
-var togglePopupMeals = function () {
-    if (popupMeals.className === "popup pshow" || overlayMeals.className === "overlay pshow") {
-        popupMeals.className = "popup";
-        overlayMeals.className = "overlay";
-    } else {
-        popupMeals.className = "popup pshow";
-        overlayMeals.className = "overlay oshow";
-        
-        // close all others
-        plus.className = "menuicon";
-        popup.className = "popup";
-        overlay.className = "overlay";
-        popupPersons.className = "popup";
-        overlayPersons.className = "overlay";
-        popupFlavors.className = "popup";
-        overlayFlavors.className = "overlay";
-    }
-};
-
-var togglePopupFlavors = function () {
-    if (popupFlavors.className === "popup pshow" || overlayFlavors.className === "overlay pshow") {
-        popupFlavors.className = "popup";
-        overlayFlavors.className = "overlay";
-    } else {
-        popupFlavors.className = "popup pshow";
-        overlayFlavors.className = "overlay oshow";
-        
-        // close all others
-        plus.className = "menuicon";
-        popup.className = "popup";
-        overlay.className = "overlay";
-        popupPersons.className = "popup";
-        overlayPersons.className = "overlay";
-        popupMeals.className = "popup";
-        overlayMeals.className = "overlay";
-    }
-};
-
-class RecipeItem {
-    constructor(name, owner, ingredients, instructions) {
-    this.name = name;
-    this.owner = owner;
-    this.ingredients = ingredients;
-    this.instructions = instructions;
-    }  
-}
-
-function deleteItem1(input, index) {
+function deleteRecipe(input, index) {
     var element = input;
     element.remove();
     user1.recipesArr.splice(index, 1);
@@ -469,7 +349,7 @@ function displayAllRecipes() {
                 "<h3 class = \"expand\">&#8964;</h3>" + 
             "</td>" +
 
-            "<td style = \"width: 7%; text-align: center\" onclick = \"deleteItem1(this.parentElement," + i + ")\">" +
+            "<td style = \"width: 7%; text-align: center\" onclick = \"deleteRecipe(this.parentElement," + i + ")\">" +
                 "<h3>x</h3>" +
             "</td>" +
         "</tr>";
